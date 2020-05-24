810f1d9600a0c87e83584450b80b16c4a902e7d8
Jorge Vilca <jorge.vilca@rimac.com.pe>
2020-05-24 17:56:00 -0500

    added firebase configuration

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ import envalid from "envalid";
 const { str } = envalid;
 
 export default envalid.cleanEnv(process.env, {
-  HYDRA_OAUTH2_INTROSPECT_URL: str({ devDefault: "http://hydra:4445/oauth2/introspect" })
+  HYDRA_OAUTH2_INTROSPECT_URL: str({ devDefault: "http://hydra:4445/oauth2/introspect" }),
+  FIREBASE_DATABASE_URL: str({ devDefault: "https://<DATABASE_NAME>.firebaseio.com" }),
 }, {
   dotEnvPath: null
 });
---
+++ src/util/expandFirebaseAuthToken.js
@@ -0,0 +1,29 @@
+import * as admin from 'firebase-admin';
+import config from "../config.js";
+
+const { FIREBASE_DATABASE_URL } = config;
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: FIREBASE_DATABASE_URL
+});
+
+/**
+ * Given an Authorization Bearer token it returns a JSON object with user
+ * properties and claims found
+ *
+ * @name expandAuthToken
+ * @method
+ * @summary Expands an Auth token
+ * @param {String} token Auth token
+ * @returns {Object} JSON object
+ */
+export default async function expandAuthToken(token) {
+  try {
+    const { uid } = await admin.auth().verifyIdToken(token)
+    return { active: true, sub: uid, token_type: 'access_token' }
+  } catch(e) {
+    console.error(e)
+    throw new Error("Error introspecting token")
+  }
+}
